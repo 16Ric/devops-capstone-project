33479674858ee2a96988b981d1d6196b8a6475c6
16Ric <aloysiusric04@gmail.com>
2025-10-01 07:46:04 -0400

    flake fix

--- service/routes.py
+++ service/routes.py
@@ -54,7 +54,9 @@ def create_accounts():
     # location_url = url_for("get_accounts", account_id=account.id, _external=True)
     location_url = "/"  # Remove once get_accounts has been implemented
     return make_response(
-        jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
+        jsonify(message),
+        status.HTTP_201_CREATED,
+        {"Location": location_url}
     )
 
 ######################################################################
@@ -71,7 +73,10 @@ def list_accounts():
     accounts = Account.all()
     account_list = [account.serialize() for account in accounts]
 
-    app.logger.info("Returning [%s] accounts", len(account_list))
+    app.logger.info(
+        "Returning [%s] accounts",
+        len(account_list)
+    )
     return jsonify(account_list), status.HTTP_200_OK
 
 ######################################################################
@@ -87,7 +92,10 @@ def get_accounts(account_id):
 
     account = Account.find(account_id)
     if not account:
-        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+        abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Account with id [{account_id}] could not be found."
+        )
 
     return account.serialize(), status.HTTP_200_OK
 
@@ -100,11 +108,17 @@ def update_accounts(account_id):
     Update an Account
     This endpoint will update an Account based on the posted data
     """
-    app.logger.info("Request to update an Account with id: %s", account_id)
+    app.logger.info(
+        "Request to update an Account with id: %s",
+        account_id
+    )
 
     account = Account.find(account_id)
     if not account:
-        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+        abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Account with id [{account_id}] could not be found."
+        )
 
     account.deserialize(request.get_json())
     account.update()
@@ -120,7 +134,10 @@ def delete_accounts(account_id):
     Delete an Account
     This endpoint will delete an Account based on the account_id that is requested
     """
-    app.logger.info("Request to delete an Account with id: %s", account_id)
+    app.logger.info(
+        "Request to delete an Account with id: %s",
+        account_id
+    )
 
     account = Account.find(account_id)
     if account:
@@ -138,7 +155,10 @@ def check_content_type(media_type):
     content_type = request.headers.get("Content-Type")
     if content_type and content_type == media_type:
         return
-    app.logger.error("Invalid Content-Type: %s", content_type)
+    app.logger.error(
+        "Invalid Content-Type: %s",
+        content_type
+    )
     abort(
         status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
         f"Content-Type must be {media_type}",
